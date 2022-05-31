e40403b588fd9d92267feb0a965c2626c80546da
Quinn <quinnpudenz@gmail.com>
2022-05-30 21:24:45 -0500

    Finished the framework of a round of rock, paper, scissors using nested if statements. Cleaned up previous work.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,40 +1,72 @@
-// Create an array of possible choices in the game.//
+// Completed - GOAL 1: Create a function that plays a single round of Rock Paper Scissors, 
+// using computerSelection and playerSelection as parameters. //
+// Completed - GOAL 2: Return a string that declares a winner. //
+
+// Create an array of possible choices in the game. //
 
 var choices = ["rock", "paper", "scissors"];
 
-// Create a function to randomly select a choice from the array on behalf of the computer player.//
+// Create a function to randomly select a choice from the array on behalf of the computer player. //
+
+const computerSelection = computerPlay(); // I could probably avoid this line by renaming this. //
+
 
 function computerPlay() {
     return (choices[Math.floor(Math.random() * 3)]);
 };
 
-// GOAL 1: Create a function that plays a single round of Rock Paper Scissors, using computerSelection and playerSelection parameters.//
-// GOAL 2: Return a string that declares a winner.//
+// We need something that gets the input ready for the playRound function. //
+
+const playerSelection = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        return userInput;
+    } else {
+        console.log('Error, please select type rock, paper, or scissors.')
+    };
+}
+
+console.log(playerSelection('Rock')); // To test .toLowerCase. //
+console.log(playerSelection('potato')); // To test error message. //
+console.log(computerSelection); // Log of computer's selection to make sure next test was correct. //
+console.log(playRound(playerSelection('Rock'), computerSelection)); // Test outcome based one playerSelection being rock. I still need to figure out how to input other choices. //
+
+// Considering my options at this point, it seems like I an use if statements and || to solve my problem. It seems like switch is another way to achieve this. I'm not sure how to implement .equals, but that might help clean things up as well. //
+// When considering if statements, I still have options: I can base it on playerSelection, the outcomes, or even associate each playerSelection with a losing condition. //
 
 
-// Considering my options at this point, it seems like I an use if statements and || to solve my problem. It seems like switch is another way to achieve this. I'm not sure how to implement .equals, but that might help clean things up as well.//
-// When considering if statements, I still have options: I can base it on playerSelection, the outcomes, or even associate each playerSelection with a losing condition.//
+// For now, I'm going to group if statements starting with the player's selection, using the function playRound. //
+function playRound(userInput, computerSelection) {   
 
-
-// For now, I'm going to group if statements starting with the player's selection.//
-// I'll map out the outcomes starting with rock, like the example below, to make sure my javascript logic is sound.//
-function playRound(playerSelection, computerSelection) {
-        if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
-            if (playerSelection === computerSelection) {
+            if (userInput === computerSelection) {
                 console.log("Tie.");
-                } 
-            else if (playerSelection === "rock" && computerSelection === "paper") {
+                }
+
+            else if (userInput === "rock") {
                 if (computerSelection === "paper") {
-                    console.log("You lose.")
+                    console.log("You lose.");
+                    }
+                else if (computerSelection === "scissors") {
+                    console.log("You win.");
                     }
             }
-                else if (playerSelection === "rock" && computerSelection === "scissors") {
-                    console.log("You win.")
-                    }
-                }
-        }
 
-// To test the current setup, I've set playSelection to output "rock". In the future, I could setup playerSelection similar to computerSelection to test different outcomes.//
-let playSelection = "rock";
-const computerSelection = computerPlay();
-playRound(playSelection, computerSelection);
+            else if (userInput === "paper") {
+                if (computerSelection === "rock") {
+                    console.log("You win.");
+                }
+                else if (computerSelection === "scissors") {
+                    console.log("You lose.");
+                }
+            }
+
+            else if (userInput === "scissors") {
+                if (computerSelection === "rock") {
+                    console.log("You lose.");
+                }
+                else if (computerSelection === "paper") {
+                    console.log("You win.")
+                }
+            }
+    
+        }
